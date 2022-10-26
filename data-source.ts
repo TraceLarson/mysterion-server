d5e50ffbe8c1bbee8d3e42d3aca3096cf1eba5a4
trace larson <trace.larson4917@gmial.com>
2022-10-26 13:01:31 -0500

    added PerformanceData and RawData to entities in data-souce

--- data-source.ts
+++ data-source.ts
@@ -3,6 +3,8 @@ import { DataSource } from "typeorm";
 import { Lease } from "./persistence/entity/Lease";
 import { Manager } from "./persistence/entity/Manager";
 import { Operator } from "./persistence/entity/Operator";
+import { PerformanceData } from "./persistence/entity/PerformanceData";
+import { RawData } from "./persistence/entity/RawData";
 import { User } from "./persistence/entity/User";
 import { Well } from "./persistence/entity/Well";
 import { WellTraits } from "./persistence/entity/WellTraits";
@@ -16,7 +18,7 @@ export const AppDataSource = new DataSource({
   database: "test",
   synchronize: true,
   logging: false,
-  entities: [Lease, Manager, Operator, User, Well, WellTraits],
+  entities: [Lease, Manager, Operator, PerformanceData, RawData, User, Well, WellTraits],
   migrations: [],
   subscribers: [],
 });
